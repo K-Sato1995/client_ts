c37334e1b61a7264ba37ac1c46e2155ed04772a5
佐藤克樹 <k-sato@ruby-dev.jp>
2019-06-04 11:35:30 +0900

    Remove unnecerally files

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,6 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react";
 
-import logo from './logo.svg';
-
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return <div className="App" />;
 }
-
 export default App;
