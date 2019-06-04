a99c6a292434c0614d1db0ab2864a508cb04d17c
佐藤克樹 <k-sato@ruby-dev.jp>
2019-06-04 12:58:53 +0900

    Add material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import Button from "@material-ui/core/Button";
 import * as React from "react";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary">
+        Test
+      </Button>
+    </div>
+  );
 }
 export default App;
