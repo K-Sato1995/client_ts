8dc91f1ba2b0ac6eb1e670e8c364cd372bd00605
佐藤克樹 <k-sato@ruby-dev.jp>
2019-06-17 14:14:43 +0900

    Back to the previous state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,6 @@
 import Button from "@material-ui/core/Button";
 import * as React from "react";
-import { gql } from "apollo-boost";
-import { Query } from "react-apollo";
 
-const Posts = () => {
-  <Query
-    query={gql`
-      query {
-        posts {
-          id
-          title
-          description
-        }
-      }
-    `}
-  >
-    {(loading, error, data)} => {
-      if(loading) return <p>Loading</p>;
-      return data.posts
-    }
-  </Query>
-};
 const App: React.FC = () => {
   return (
     <div className="App">
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,4 +1,19 @@
 import ApolloClient from "apollo-boost";
+import { gql } from "apollo-boost";
 export const client = new ApolloClient({
   uri: "http://localhost:3000/graphql"
 });
+
+client
+  .query({
+    query: gql`
+      query {
+        posts {
+          id
+          title
+          description
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
