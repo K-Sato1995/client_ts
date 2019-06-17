1df4f88d08679045bbe118d16cdfe8d92a10fc41
佐藤克樹 <k-sato@ruby-dev.jp>
2019-06-17 15:53:02 +0900

    Able to fetch titles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,23 @@
 import Button from "@material-ui/core/Button";
 import * as React from "react";
+import { usePostsQuery } from "../generated/graphql";
 
 const App: React.FC = () => {
+  const { data, error, loading } = usePostsQuery();
+
+  if (loading) return <div>Fetching</div>;
+  if (error) return <div>Error</div>;
+  if (!data) return <div>Data not found</div>;
+
+  const posts = data.posts.map(post => <li key={post.id}>{post.title}</li>);
+
   return (
     <div className="App">
       <h1>GraphQL Client</h1>
       <Button variant="contained" color="primary">
         Test
       </Button>
+      <ul>{posts}</ul>
     </div>
   );
 };
